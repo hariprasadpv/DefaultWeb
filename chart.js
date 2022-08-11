3bf376aed580336115ebb278b7395463568acfb8
hariprasadpv <110605665+hariprasadpv@users.noreply.github.com>
2022-08-11 16:34:59 +0530

    Update chart.js

--- chart.js
+++ chart.js
@@ -2,7 +2,7 @@
  * Chart.js v3.9.1
  * https://www.chartjs.org
  * (c) 2022 Chart.js Contributors
- * Released under the MIT License
+ * Released under the MIT License -----edited in remote repo
  */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
